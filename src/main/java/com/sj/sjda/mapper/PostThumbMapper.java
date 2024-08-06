fa99e1b33f220a741a30628c8d9b9e5913213072
main <2512325502@qq.com>
2024-08-06 15:12:46 +0800

    SjDa初始化模板

---
+++ src/main/java/com/sj/sjda/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.sj.sjda.mapper;
+
+import com.sj.sjda.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ *   
+ *  
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/sj/sjda/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.sj.sjda.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sj.sjda.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *   
+ *  
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
